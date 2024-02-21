2f7b3a0bf1b0aa5b33bf226dc903a027bcf2de58
Jesse-js <jesse-jjs@hotmail.com>
2024-02-21 12:24:51 -0300

    feat: working with loop iterations

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
-//Conditional Statements
-const num = Number(0);
-
-console.log(num);
-
-if (!num) {
-  console.log("The number is 0");
-} else if (num < 0) {
-  console.log("The number is negative");
-} else {
-  console.log("The number is positive");
+// Loops
+// For loop
+for (let i = 0; i < 10; i++) {
+    console.log(i);
 }
 
-const color = "white";
-switch (color) {
-  case "green":
-    console.log("Color is green");
-    break;
-  case "red":
-    console.log("Color is red");
-    break;
-  default:
-    console.log("Color is not red or green");
-    break;
+// While loop
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+// Do while loop
+i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i === 0)
+
+// For of 
+let arr = [1, 2, 3, 4];
+
+for (let number of arr) {
+    console.log(number);
+}
+
+// For in
+let person = {
+    name: 'John',
+    age: 30
+}
+
+for (let key in person) {
+    console.log(key, person[key]);
 }
