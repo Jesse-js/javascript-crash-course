364b5c17634fd20c0de075121082ea96bee80a0f
Jesse-js <jesse-jjs@hotmail.com>
2024-02-21 11:56:36 -0300

    feat: working with equality

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
-//Type conversion
-console.log(typeof (2 + '3'));
-console.log('4' - '10');
-console.log('4' + '10');
-console.log('4' * '10');
-console.log('4' / '10');
-console.log(5 - undefined);
-console.log('João' - null);
+//Equality
 
+const var1 = true;
+const var2 = 1;
 
-let age = "18";
-console.log(typeof age);
-age = Number(age);
-console.log(typeof age);
-
-console.log(Number('5'));
-console.log(parseInt('5'));
-console.log(parseFloat('5.5'));
-console.log(String('5.5'));
-console.log((1025).toString());
-console.log(Boolean(0)); // null, undefined, 0, '', NaN return false, everything else return true
+console.log(var1 == var2); //true
+console.log(var1 === var2); //false
