32d83e02b124a00244ea5274a7d4c8eba954f814
Jesse-js <jesse-jjs@hotmail.com>
2024-02-21 11:33:39 -0300

    feat: working with data types

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-console.log('Hello from main.js');
+//Data Types
+const name = "Jessé";
+const language = "JavaScript";
+const chanel = `Codevolution`;
 
+const total = 0;
+const PI = 3.14;
 
-let age = 20;
-console.log(age);
+const isPrimaryNumber = false;
+const result = undefined;
+const data = null;
 
-const salary = 8000;
-console.log(salary);
+const person = {
+  name: "Jessé",
+  lastName: "Jorge Santana",
+  age: 22,
+};
 
-let sum;
-console.log(sum);
+console.log(person.name);
+
+let oddNumbers = [1, 3, 5, 7];
+
+console.log(oddNumbers[3]);
+
+let a = 10;
+a = 'Teste';
+a = false;
+
+console.log(a);
